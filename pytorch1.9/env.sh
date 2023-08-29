7b733a9b3965f9d2fd277b0d12a81fb22cbe8cb3
CambriconKnight <cambricon_knight@163.com>
2023-08-29 10:28:38 +0800

    update env

--- pytorch1.9/env.sh
+++ pytorch1.9/env.sh
@@ -9,20 +9,20 @@
 # -------------------------------------------------------------------------------
 #################### version ####################
 ## 以下信息,根据各个版本中文件实际名词填写.
-#pytorch镜像的版本号，如【pytorch-v1.13.0-torch1.9-ubuntu18.04-py37.tar.gz】则取作【1.13.0】。
-VER="1.13.0"
+#pytorch镜像的版本号，如【pytorch-v1.15.0-torch1.9-ubuntu18.04-py37.tar.gz】则取作【1.15.0】。
+VER="1.15.0"
 Owner="kang"
-TestModel="-ChatGLM"
-#TestModel=""
+#TestModel="-ChatGLM"
+TestModel=""
 #################### docker ####################
 #Work
 PATH_WORK="pytorch"
 #Dockerfile(16.04/18.04/CentOS)
 OSVer="18.04"
-#Operating system，如【pytorch-v1.13.0-torch1.9-ubuntu18.04-py37.tar.gz】则取作【ubuntu18.04-py37】。
+#Operating system，如【pytorch-v1.15.0-torch1.9-ubuntu18.04-py37.tar.gz】则取作【ubuntu18.04-py37】。
 OS="ubuntu${OSVer}-py37"
 #OS="ubuntu$OSVer-py37-20230531210906-20230531220906"
-#FrameVersion，如【pytorch-v1.13.0-torch1.9-ubuntu18.04-py37.tar.gz】则取作【v1.13.0-torch1.6】。
+#FrameVersion，如【pytorch-v1.15.0-torch1.9-ubuntu18.04-py37.tar.gz】则取作【v1.15.0-torch1.9】。
 FrameVer="v${VER}-torch1.9"
 #(Dockerfile.16.04/Dockerfile.18.04/Dockerfile.CentOS)
 FILENAME_DOCKERFILE="Dockerfile.${OSVer}"
@@ -41,12 +41,12 @@ MY_IMAGE="${ORG}/${PATH_WORK}"
 NAME_IMAGE="$MY_IMAGE:$VERSION"
 #FileName DockerImage
 #FILENAME_IMAGE="$PATH_WORK-$FrameVer-$OS.tar.gz"
-#FILENAME_IMAGE="pytorch-v1.13.0-torch1.9-ubuntu18.04-py37.tar.gz"
+#FILENAME_IMAGE="pytorch-v1.15.0-torch1.9-ubuntu18.04-py37.tar.gz"
 FILENAME_IMAGE="$PATH_WORK-$FrameVer-$OS${TestModel}.tar.gz"
-#FILENAME_IMAGE="pytorch-v1.13.0-torch1.9-ubuntu18.04-py37-ChatGLM.tar.gz"
+#FILENAME_IMAGE="pytorch-v1.15.0-torch1.9-ubuntu18.04-py37-ChatGLM.tar.gz"
 FULLNAME_IMAGE="./docker/${FILENAME_IMAGE}"
 #Docker container name
-#Docker container name  container-pytorch-0.15.602-ubuntu18.04-kang
+#Docker container name  container-pytorch-v1.15.0-torch1.9-ubuntu18.04-py37-kang
 MY_CONTAINER="container-${PATH_WORK}-${VERSION}-${Owner}"
 #################### color ####################
 #Font color
